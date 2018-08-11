af93e5afddea4eb112ab642ea3f45cb752441279
Zach <zachmatuson@gmail.com>
2018-08-11 02:31:50 -0400

    Cleaned up some old comments

--- forge_injector/src/main.rs
+++ forge_injector/src/main.rs
@@ -4,13 +4,13 @@ use std::env;
 
 fn main() {
     let DLL_NAME = "HammerReforged.dll";
-    // let DLL_NAME = "HammerSyncInjected.dll";
     let TARGET_NAME = "hammer.exe";
 
-    // TODO change debug to release or find way to automate it
-    let mut dll_path = env::current_exe().unwrap(); //.to_string_lossy() + "\\target\\debug\\" + DLL_NAME).to_string();
+    // TODO change dll filepath to be independent of EXE path
+    let mut dll_path = env::current_exe().unwrap();
     dll_path.set_file_name(DLL_NAME);
     let dll_path = dll_path.to_str().unwrap();
+    
     let mut injector = Injector::new(TARGET_NAME, &dll_path);
     match injector.inject() {
         false => std::process::exit(1),
